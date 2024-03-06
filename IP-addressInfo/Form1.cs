4e00f827204a49d51bc95c006f3fbdf7e1e1aa67
ZellosMet <zellossmet@gmail.com>
2024-03-06 21:43:36 +0900

    1

--- IP-addressInfo/Form1.cs
+++ IP-addressInfo/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace IP_addressInfo
 {
-	public partial class Form1 : Form //
+	public partial class Form1 : Form
 	{
 		int[] ip_address = new int[4];
 		int[] mask = new int[4];
